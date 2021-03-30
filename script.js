1f9f8e92e661eab913ff712e0fff58137e2bfa64
Aminesanhaji <thpamine@gmail.com>
2021-03-30 09:37:13 +0200

    results card done

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function myFunction() {
     document.getElementById("accordionExample").style.display = "block";
     document.getElementById("Mycollapse").style.display = "none";
+    var x = document.getElementById("playerone").value;
+    document.getElementById("teamone").innerHTML = x;
+    var x = document.getElementById("playertwo").value;
+    document.getElementById("teamtwo").innerHTML = x;
+    var x = document.getElementById("playerthree").value;
+    document.getElementById("teamthree").innerHTML = x;
+    var x = document.getElementById("playerfour").value;
+    document.getElementById("teamfour").innerHTML = x;
 }
 
 
